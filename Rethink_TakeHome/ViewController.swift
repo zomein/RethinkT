d279b827112f15946f6d2b60aa1a57e598f40c66
ebarnes <ebarnes@caps.ua.edu>
2022-09-23 11:00:55 -0400

    Initial Commit

---
+++ Rethink_TakeHome/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Rethink_TakeHome
+//
+//  Created by Eric Barnes on 9/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .lightGray
+    }
+
+
+}
+
